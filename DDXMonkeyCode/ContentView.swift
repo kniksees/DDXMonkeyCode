65f6c5a3cbd6c78909db61803b54eeecf4bec490
Dmitry Erofeev <56806136+kniksees@users.noreply.github.com>
2024-06-04 22:38:49 +0300

    Initial Commit

---
+++ DDXMonkeyCode/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DDXMonkeyCode
+//
+//  Created by Dmitry Erofeev on 04.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DDXMonkeyCode/DDXMonkeyCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DDXMonkeyCodeApp.swift
+//  DDXMonkeyCode
+//
+//  Created by Dmitry Erofeev on 04.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct DDXMonkeyCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
